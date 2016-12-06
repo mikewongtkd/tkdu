0c3af98fade8bc6480914289caca6fd901ee8c1f
Mike Wong <mikewong899@me.com>
2016-12-06 02:27:06 -0800

    Added certificate border kindly put into the public domain by Arvin. Will probably generate customized SVG certificate, possibly with QR code verification URL.

--- backend/lib/TKDU/RequestHandler.pm
+++ backend/lib/TKDU/RequestHandler.pm
@@ -1,4 +1,10 @@
 package TKDU::RequestHandler;
+use Clone qw( clone );
+use Data::Dumper;
+use Data::Structure::Util qw( unbless );
+use Digest::SHA1 qw( sha1_hex );
+use File::Slurp;
+use Try::Tiny;
 
 # ============================================================
 sub new {
@@ -13,6 +19,35 @@ sub new {
 sub init {
 # ============================================================
 	my $self = shift;
+}
+
+# ============================================================
+sub handle {
+# ============================================================
+	my $self    = shift;
+	my $request = shift;
+	{
+		read  => \&handle_read,
+		write => \&handle_write,
+	}->{ $request->{ action }}( $self, $request );
+}
+
+# ============================================================
+sub handle_read {
+# ============================================================
+	my $self    = shift;
+	my $request = shift;
+	my $db      = "/usr/local/tkdu/db/qa/$request->{ subject }.json";
+
+	die "Course subject '$request->{ subject }' not found in database" unless -e $db;
+	return read_file( $db )
+}
+
+# ============================================================
+sub handle_write {
+# ============================================================
+	my $self    = shift;
+	my $request = shift;
 }
 
 1;
